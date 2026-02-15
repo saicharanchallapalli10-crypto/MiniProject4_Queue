43f8ac6b3ad73d5520ca1fe288bd5e301262639d
Saicharan Challapalli <joetillmar4@gmail.com>
2026-02-14 21:00:30 -0500

    Add brief method comments to Queue.h

--- include/Queue.h
+++ include/Queue.h
@@ -8,15 +8,28 @@ private:
     int rearIndex;
     int count;
 public:
+
+//constructor used to intialize an empty queue
     Queue();
+
+//Adds an item to the end of an queue
 void enqueue(int item);
+
+//"Removes" ad returns the first item in the queue
 int dequeue();
+
+//Tells the user what the front item in the queue is 
 int peek() const;
 
+//returns "true" if the queue has no values in it 
 bool isEmpty() const;
+
+//return "true" if the queue is full
 bool isFull() const;
 
+//returns number of items in the queue
 int size() const;
+//Removes all the items in queue, basically like a reset button
 void clear();
 };
 
